4e3b6decdf5c5b7ff66bc4415e886ea30b695d0c
dbyl <bylicki.damian@gmail.com>
2022-11-13 18:59:09 +0100

    creating no-null csv dataset file, setting up first model

--- source/configuration/spotify_data/models.py
+++ source/configuration/spotify_data/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class SpotifyData:
+    
+        title = models.CharField(max_length=200)
+        rank = models.DecimalField(max_digits=5, decimal_places=2)
+        date =  models.DateTimeField()
+        artist = models.CharField(max_length=200)
+        url = models.CharField(max_length=200)
+        region = models.CharField(max_length=50)
+        chart = models.CharField(max_length=15)
+        streams = models.BigAutoField()
