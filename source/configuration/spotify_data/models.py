492c5449a8b6863e3c430d867f03d6474379fbda
dbyl <bylicki.damian@gmail.com>
2022-11-15 18:43:20 +0100

    creating admin site, exploring admin site, editing models

--- source/configuration/spotify_data/models.py
+++ source/configuration/spotify_data/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class SpotifyData(models.Model):
     
         title = models.CharField(max_length=200)
-        rank = models.DecimalField(max_digits=5, decimal_places=2)
-        date =  models.DateTimeField()
+        rank = models.DecimalField(max_digits=5, decimal_places=0)
+        date =  models.DateField()
         artist = models.CharField(max_length=200)
         url = models.CharField(max_length=200)
         region = models.CharField(max_length=50)
         chart = models.CharField(max_length=15)
-        streams = models.DecimalField(max_digits=50, decimal_places=2)
+        streams = models.DecimalField(max_digits=50, decimal_places=0)
         
