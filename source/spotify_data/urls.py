e8ee159495cbc7bb289d7330b81c4afe1258b8ce
dbyl <bylicki.damian@gmail.com>
2023-01-27 16:49:41 +0100

    Adding dropdown for the navbar, updating templates

--- source/spotify_data/urls.py
+++ source/spotify_data/urls.py
@@ -20,24 +20,24 @@ from spotify_data.views import (
 
 urlpatterns = [
     path("", HomeView.as_view(template_name="index.html"), name="home"),
-    path("rankchart/", RankChart.as_view(template_name="rank_chart.html"), \
+    path("rankchart/", RankChart.as_view(template_name="charts/rank_chart.html"), \
     name="rank_chart"),
-    path("rankchart2/", RankChart2.as_view(template_name="rank_chart_2.html"), \
+    path("rankchart2/", RankChart2.as_view(template_name="charts/rank_chart_2.html"), \
     name="rank_chart_2"),
-    path("populrank/", PopularityChart.as_view(template_name="popularity_chart.html"), \
+    path("populrank/", PopularityChart.as_view(template_name="charts/popularity_chart.html"), \
     name="popul_chart"),
-    path("populrank2/", PopularityChart2.as_view(template_name="popularity_chart_2.html"), \
+    path("populrank2/", PopularityChart2.as_view(template_name="charts/popularity_chart_2.html"), \
     name="popul_chart_2"),
-    path("artistmappopul/", ArtistMapPopularity.as_view(template_name="artist_map_popularity.html"), \
+    path("artistmappopul/", ArtistMapPopularity.as_view(template_name="charts/artist_map_popularity.html"), \
     name="artist_map_popul"),
-    path("songmappopul/", SongMapPopularity.as_view(template_name="song_map_popularity.html"), \
+    path("songmappopul/", SongMapPopularity.as_view(template_name="charts/song_map_popularity.html"), \
     name="song_map_popul"),
-    path("topstreamedart/", TopStreamedArtistsChart.as_view(template_name="top_streamed_artists_chart.html"), \
+    path("topstreamedart/", TopStreamedArtistsChart.as_view(template_name="charts/top_streamed_artists_chart.html"), \
     name="top_streamed_artists_chart"),
-    path("topstreamedart2/", TopStreamedArtistsChart2.as_view(template_name="top_streamed_artists_chart_2.html"), \
+    path("topstreamedart2/", TopStreamedArtistsChart2.as_view(template_name="charts/top_streamed_artists_chart_2.html"), \
     name="top_streamed_artists_chart_2"),
-    path("topstreamedsongs/", TopStreamedSongsChart.as_view(template_name="top_streamed_songs_chart.html"), \
+    path("topstreamedsongs/", TopStreamedSongsChart.as_view(template_name="charts/top_streamed_songs_chart.html"), \
     name="top_streamed_songs_chart"),
-    path("topstreamedsongs2/", TopStreamedSongsChart2.as_view(template_name="top_streamed_songs_chart_2.html"), \
+    path("topstreamedsongs2/", TopStreamedSongsChart2.as_view(template_name="charts/top_streamed_songs_chart_2.html"), \
     name="top_streamed_songs_chart_2"),
 ]
