5dcfa0a289cc7fdf1e487e9ff897723e8672b1fe
dbyl <bylicki.damian@gmail.com>
2023-02-03 12:38:47 +0100

    Creating register template, adding css, cosmetic changes to the visual side of subpages

--- source/spotify_data/forms.py
+++ source/spotify_data/forms.py
@@ -204,6 +204,11 @@ class TopStreamedSongsForm2(forms.Form):
 
 class CreateUserForm(UserCreationForm):
 
+    username = forms.CharField(widget=forms.TextInput(attrs={'type':'charfield', 'class':'form_widgets'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'type':'charfield', 'class':'form_widgets'}))
+    password1 = forms.CharField(label='Password', widget=forms.TextInput(attrs={'type':'password', 'class':'form_widgets'}))
+    password2 = forms.CharField(label='Repeat Password', widget=forms.TextInput(attrs={'type':'password', 'class':'form_widgets'}))
+
     class Meta:
         model = User 
         fields = ["username", "email", "password1", "password2"]
